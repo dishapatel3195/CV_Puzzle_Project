e6d068db1606b2594aa15bcc34469d9a5b54bd27
dishapatel3195 <dishapatel3195@gmail.com>
2026-04-16 19:41:50 -0400

    image splitter adder

---
+++ grid_splitter.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import os
+import random
+
+def split_grid(image_path, rows, cols, output_folder):
+    img = cv2.imread(image_path)
+    h, w, _ = img.shape
+    
+    piece_h = h // rows
+    piece_w = w // cols
+    
+    os.makedirs(output_folder, exist_ok=True)
+    
+    pieces = []
+    count = 0
+    
+    for i in range(rows):
+        for j in range(cols):
+            piece = img[i*piece_h:(i+1)*piece_h,
+                        j*piece_w:(j+1)*piece_w]
+            
+            cv2.imwrite(f"{output_folder}/{count}.png", piece)
+            pieces.append(piece)
+            count += 1
+    
+    return pieces
+
+img = "brutus_truth.png"
+output_folder = "brutus_puzzle_pieces"
+rows, cols = 4, 4
+split_grid(img, rows, cols, output_folder)
