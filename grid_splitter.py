2334a16e2bd09a6a09ed627bd207831c15bcaecc
dishapatel3195 <dishapatel3195@gmail.com>
2026-04-16 20:02:28 -0400

    all images broken to puzzle pieces

--- grid_splitter.py
+++ grid_splitter.py
@@ -17,16 +17,25 @@ def split_grid(image_path, rows, cols, output_folder):
     
     for i in range(rows):
         for j in range(cols):
-            piece = img[i*piece_h:(i+1)*piece_h,
-                        j*piece_w:(j+1)*piece_w]
-            
+            piece = img[i*piece_h:(i+1)*piece_h, j*piece_w:(j+1)*piece_w]
             cv2.imwrite(f"{output_folder}/{count}.png", piece)
             pieces.append(piece)
             count += 1
     
     return pieces
 
+
 img = "brutus_truth.png"
 output_folder = "brutus_puzzle_pieces"
 rows, cols = 4, 4
 split_grid(img, rows, cols, output_folder)
+
+img2 = "japan_truth.png"
+output_folder = "japan_puzzle_pieces"
+rows, cols = 4, 4
+split_grid(img2, rows, cols, output_folder)
+
+img2 = "cookies_truth.png"
+output_folder = "cookies_puzzle_pieces"
+rows, cols = 4, 4
+split_grid(img2, rows, cols, output_folder)
